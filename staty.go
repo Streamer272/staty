4d40fb605121bef78f65900e45d7142a33f606bc
Streamer272 <daniel.svitan.team7274dev@gmail.com>
2022-03-25 20:19:10 +0100

    🔨 Refactors `interface{}` to `any`

--- staty.go
+++ staty.go
@@ -1,19 +1,19 @@
 package staty
 
 type Store struct {
-	state map[string]interface{}
+	state map[string]any
 }
 
 func NewStore() *Store {
 	return &Store{
-		state: map[string]interface{}{},
+		state: map[string]any{},
 	}
 }
 
-func (s *Store) Set(name string, value interface{}) {
+func (s *Store) Set(name string, value any) {
 	s.state[name] = value
 }
 
-func (s *Store) Get(name string) interface{} {
+func (s *Store) Get(name string) any {
 	return s.state[name]
 }
